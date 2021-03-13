5dc986288eb4dfb08028a525811253c2e52bcb89
njmercado <njmercado@uninorte.edu.co>
2021-03-12 19:08:21 -0500

    Agregada vista del dashboard

--- src/app/dashboard-view/dashboard-view.component.ts
+++ src/app/dashboard-view/dashboard-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import Charge from '../../utils/charges'
+const charge = new Charge('http://localhost:3000/charge')
 
 @Component({
   selector: 'app-dashboard-view',
@@ -10,6 +12,21 @@ export class DashboardViewComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.role = localStorage.getItem("role")
+    this.charges = JSON.parse(localStorage.getItem("charges"))
   }
 
+  role: string = ""
+  charges = []
+
+  async deleteCharge(data) {
+    const result = await charge.delete({
+      charge_id: data.charge_id,
+      email: data.email,
+      token: localStorage.getItem("token")
+    })
+    if(!result.error){
+      this.charges = this.charges.filter(item => item.charge_id !== data.charge_id)
+    }
+  }
 }
