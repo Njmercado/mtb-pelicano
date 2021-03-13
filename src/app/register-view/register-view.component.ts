74472386326fcf6ae8f708d958f269289a5544fc
njmercado <njmercado@uninorte.edu.co>
2021-03-12 19:11:13 -0500

    Agregado formulario

--- src/app/register-view/register-view.component.ts
+++ src/app/register-view/register-view.component.ts
@@ -1,4 +1,7 @@
+import { Router } from '@angular/router'
 import { Component, OnInit } from '@angular/core';
+import User from '../../utils/user'
+const user = new User('http://localhost:3000/register')
 
 @Component({
   selector: 'app-register-view',
@@ -7,11 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  email: ""
+  email = ""
+  document_number= ""
+  name = ""
+  last_name = ""
+  phone = ""
+  role = ""
+  password = ""
+  error = false
+  errorMessage = ""
 
+  //Functions
+  async signUp() {
+    const response = await user.signUp({
+      email: this.email,
+      document_number: this.document_number,
+      name: this.name,
+      last_name: this.last_name,
+      phone: this.phone,
+      role: this.role,
+      password: this.password,
+    })
+
+    if(!response.error) this.router.navigate(['/dashboard'])
+    else{
+      this.error = true
+      this.errorMessage = response.message
+    }
+
+  }
+
+  logIn() {
+    this.router.navigate(['/login'])
+  }
 }
